e7f82f03abd7ea80efc888e3c483f8ab46dc51f2
root <root@NOD-BF1-DO-H017.(none)>
2015-07-24 15:56:59 +0530

    chnages data

--- common/extensions/adminui/themes/site/index.php
+++ common/extensions/adminui/themes/site/index.php
@@ -18,100 +18,15 @@ BootstrapLoaderAsset::register($this);
 
 ?>
 <div class="row">
-    <div class="col-lg-3 col-xs-6">
-        <!-- small box -->
-        <div class="small-box bg-aqua">
-            <div class="inner">
-                <h3>1</h3>
-                <p>T</p>
-            </div>
-            <div class="icon">
-                <i class="ion ion-bag"></i>
-            </div>
-            <a href="#" class="small-box-footer">
-                More info <i class="fa fa-arrow-circle-right"></i>
-            </a>
-        </div>
-    </div><!-- ./col -->
-    <div class="col-lg-3 col-xs-6">
-        <!-- small box -->
-        <div class="small-box bg-green">
-            <div class="inner">
-                <h3>
-                    1
-                </h3>
-                <p>
-                   T
-                </p>
-            </div>
-            <div class="icon">
-                <i class="ion ion-stats-bars"></i>
-            </div>
-            <a href="#" class="small-box-footer">
-                More info <i class="fa fa-arrow-circle-right"></i>
-            </a>
-        </div>
-    </div><!-- ./col -->
-    <div class="col-lg-3 col-xs-6">
-        <!-- small box -->
-        <div class="small-box bg-yellow">
-            <div class="inner">
-                <h3>
-                    1
-                </h3>
-                <p>
-                   T 
-                </p>
-            </div>
-            <div class="icon">
-                <i class="ion ion-person-add"></i>
-            </div>
-            <a href="#" class="small-box-footer">
-                More info <i class="fa fa-arrow-circle-right"></i>
-            </a>
-        </div>
-    </div><!-- ./col -->
-    <div class="col-lg-3 col-xs-6">
-        <!-- small box -->
-        <div class="small-box bg-red">
-            <div class="inner">
-                <h3>
-                   1
-                </h3>
-                <p>
-                    T
-                </p>
-            </div>
-            <div class="icon">
-                <i class="ion ion-pie-graph"></i>
-            </div>
-            <a href="#" class="small-box-footer">
-                More info <i class="fa fa-arrow-circle-right"></i>
-            </a>
-        </div>
-    </div><!-- ./col -->
+   
 </div><!-- /.row -->
+
+
+
+
 <div class="row">
     <div class="col-md-12">
-    <?php 
     
-        Box::begin([
-            'type' => 'box-solid bg-light-blue',
-            'header' => "Quick Access",
-            'headerIcon' => 'fa fa-gear',
-            'class'=>'bg-maroon',
-            'headerButtonGroup' => true,
-        ]);
-        //echo Html::a('<i class="fa fa-file-text-o"></i> Content',['/content/content/index'],['class'=>'btn btn-app']);
-       // echo Html::a('<i class="fa fa-heart"></i> Event',['/event/event/index'],['class'=>'btn btn-app']);
-        //echo Html::a('<i class="fa fa-home"></i> Venue',['/venue/venue/index'],['class'=>'btn btn-app']);
-        //echo Html::a('<i class="fa fa-tag"></i> Tags',['/tags/tags/tag-search'],['class'=>'btn btn-app']);
-        //echo Html::a('<i class="fa fa-android"></i> Android App',['/downloads/WhatsHot_v1.0.10.apk'],['class'=>'btn btn-app']);        
-        
-    ?>
-    <?php
-        Box::end();
-    ?>
     </div>    
     
 </div>
